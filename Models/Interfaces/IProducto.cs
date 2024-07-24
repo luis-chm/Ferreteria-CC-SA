7dc31df4a2167eb4747434d60d8adef3613b7590
David Jesus Cerdas Romero <dcerdasr@edu.upolitecnica.cr>
2024-07-23 23:01:39 -0600

    Clase Producto e Interfaz

---
+++ Models/Interfaces/IProducto.cs
@@ -0,0 +1,11 @@
+﻿namespace Ferreteria_CC_SA.Models
+{
+    public interface IProducto
+    {
+        int IDProducto { get; set; }
+        string Nombre { get; set; }
+        string Descripcion { get; set; }
+        decimal Precio { get; set; }
+        int Cantidad { get; set; }
+    }
+}
---
+++ Models/Producto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Ferreteria_CC_SA.Models.Producto;
+
+namespace Ferreteria_CC_SA.Models
+{
+
+    public class Producto : IProducto
+    {
+        public int IDProducto { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public int Cantidad { get; set; }
+
+        public Producto() { }
+
+        public Producto(int idproducto, string nombre, string descripcion, decimal precio, int cantidad)
+        {
+            IDProducto = idproducto;
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Precio = precio;
+            Cantidad = cantidad;
+        }
+
+    }
+}
